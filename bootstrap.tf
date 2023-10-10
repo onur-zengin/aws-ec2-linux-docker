3c3210cf08f8ad5bab05970bac80eb56846685b3
oz <zengin.onur@gmail.com>
2023-10-10 10:42:27 +0300

    updated bootstrap

--- bootstrap.tf
+++ bootstrap.tf
@@ -16,7 +16,7 @@ data "cloudinit_config" "config" {
         default_user:
           groups: [docker]
       write_files:
-        - path: /compose.yml
+        - path: /usr/local/lib/docker/compose.yml
           encoding: b64
           content: ${base64encode(file("${path.module}/configs/compose.yml"))}
           owner: root:root
@@ -27,15 +27,20 @@ data "cloudinit_config" "config" {
           owner: root:root
           permissions: '0644'
       runcmd:
+        - echo "## Updating system $(hostnamectl | grep Kernel)" 
         - yum update -y
+        - echo "## Installing Docker" 
         - yum install docker -y
         - systemctl start docker
         - systemctl enable docker
+        - echo "## Installing the Compose plugin" 
         - sudo mkdir -p /usr/local/lib/docker/cli-plugins/
         - sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
         - sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+        - echo "## Booting up containers" 
+        - cd /usr/local/lib/docker/
         - docker compose up -d 
-      final_message: "The system is up after $UPTIME seconds"
+      final_message: "## The system is up after $UPTIME seconds"
     EOF
   }
 }
