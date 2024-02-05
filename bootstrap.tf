44dc145db27c3d30d5fdb9bf2274bf052d879dc7
oz <zengin.onur@gmail.com>
2024-02-05 14:08:05 +0300

    updated bootstrap config

--- bootstrap.tf
+++ bootstrap.tf
@@ -21,10 +21,8 @@ data "cloudinit_config" "config" {
         - name: docker
           lock_passwd: true
           gecos: Container Manager
-          #groups: [root]
           sudo: ["ALL=(ALL) NOPASSWD:ALL"]
           shell: /bin/bash
-          homedir: /etc/docker
       # Cloud-init documentation confirms that disk definitions for AWS not yet implemented at the time of writing. https://cloudinit.readthedocs.io/en/23.2.2/reference/examples.html
       # fs_setup:
       #  - filesystem: xfs
@@ -79,7 +77,7 @@ data "cloudinit_config" "config" {
       # runcmd runs only during first boot, after bootcmd.
         - [ echo, "## Mounting data volume" ]
         # If there is an existing filesystem on the data volume, mkfs (by default) will detect it & skip.
-        - [ export, EBS_DEVICE_NAME, "=", "$(lsblk | grep disk | grep -v 8G | awk {'print $1'})" ]
+        - [ export, EBS_DEVICE_NAME=, "$(lsblk | grep disk | grep -v 8G | awk {'print $1'})" ]
         - [ mkfs, -t, xfs, /dev/$EBS_DEVICE_NAME ]
         - [ xfs_admin, -L, data, /dev/$EBS_DEVICE_NAME ]
         - [ mkdir, /data ]
