ee067862299a3a2d9743729d9e932f60c745eee8
oz <zengin.onur@gmail.com>
2024-02-05 10:22:14 +0300

    updated boot conf

--- bootstrap.tf
+++ bootstrap.tf
@@ -78,7 +78,7 @@ data "cloudinit_config" "config" {
       # runcmd runs only during first boot, after bootcmd.
         - [ echo, "## Mounting data volume" ]
         # If there is an existing filesystem on the data volume, mkfs (by default) will detect it & skip.
-        - [ export, EBS_DEVICE_NAME=$(lsblk | grep disk | grep -v 8G | awk {'print $1'}) ]
+        - [ export, "EBS_DEVICE_NAME=$(lsblk | grep disk | grep -v 8G | awk {'print $1'})" ]
         - [ mkfs, -t, xfs, /dev/$EBS_DEVICE_NAME ]
         - [ xfs_admin, -L, data, /dev/$EBS_DEVICE_NAME ]
         - [ mkdir, /data ]
